8400b1956b5304c47b8d50b774390ccb215b7371
Nassar <nassar.raheem@yahoo.com>
2015-06-27 17:15:20 -0400

    modified CartService added 5c

--- client/app/cartService/cartService.service.js
+++ client/app/cartService/cartService.service.js
@@ -3,4 +3,44 @@
 angular.module('project3App')
   .service('cartService', function () {
     // AngularJS will instantiate a singleton by calling "new" on this function
+
+var that = this;
+
+  that.cart = [];
+
+  function findItemById(items, id) {
+    return _.find(items, function(item) {
+      return item._id === id;
+    });
+  }
+
+  that.addItem = function(item) {
+    var found = findItemById(that.cart, item._id);
+    if (found) {
+      found.qty += item.qty;
+    }
+    else {
+      that.cart.push(angular.copy(item));
+    }
+  };
+
+  that.removeItem = function(item) {
+    var index = that.cart.indexOf(item);
+    that.cart.splice(index, 1);
+  };
+
+  that.getCost = function(item) {
+    return item.qty * item.price;
+  };
+
+  that.getTotal = function() {
+    return _.reduce(that.cart, function(sum, item) {
+      return sum + that.getCost(item);
+    }, 0);
+  };
+
+  that.clearCart = function() {
+    that.cart.length = 0;
+  };
+
   });
